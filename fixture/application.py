9be1fec1f1e36c58aa013baf04afcc7efd046d83
Marat Sharipov <83124202+SharipovMS@users.noreply.github.com>
2021-06-29 11:36:17 +0300

    test

---
+++ fixture/application.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+class Application:
+    def __init__(self, browser, base_url): #Запуск браузера
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.session = SessionHelper(self)
+
+    def open_page_add_new(self):
+        # Открывает страницу
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def open_page(self):
+        # Открывает страницу
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
